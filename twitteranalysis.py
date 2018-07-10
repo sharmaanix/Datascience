f98ed449059038ba05c1de17302a614bd957ff32
sharmaanix <35564460+sharmaanix@users.noreply.github.com>
2018-07-10 13:42:14 +0530

    Update twitteranalysis.py

--- twitteranalysis.py
+++ twitteranalysis.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 	'lwrVsBgqg1MzXItVtrmKAsaLh'
-consumer_secret = 'AsAwjFaYkFho7LilVjyEeSde2zlTPFFDDL9s8GRwVoTVCe8ogd'
+consumer_key = 	'CONSUMER_KEY_CODE'
+consumer_secret = 'CONSUMER_SECRET_CODE'
 
-access_token = '1255986152-RBz1mmHC0lgX3FYfxIIMuGnxgof0GQIFjjT9kiq'
-access_token_secret = '0kg8M0J2GBOOXiUtBOo1Q34OP90WDq2MCiNGT6NqjB07Y'
+access_token = 'ACCESS_TOKEN_CODE'
+access_token_secret = 'ACCESS_TOKEN_SECRET_CODE'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
